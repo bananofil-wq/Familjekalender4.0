7fbd2884fdba68d26fe8e137d24981e4df69bba7
bananofil-wq <bananofil@gmail.com>
2026-09-10 21:41:12 +0200

    Fix RoundedCornerShape compile reference

---
+++ app/src/main/java/se/familjekalender/app/RoundedCornerShapeCompat.kt
@@ -0,0 +1,6 @@
+package se.familjekalender.app
+
+import androidx.compose.foundation.shape.RoundedCornerShape as ComposeRoundedCornerShape
+import androidx.compose.ui.unit.Dp
+
+internal fun RoundedCornerShape(size: Dp) = ComposeRoundedCornerShape(size)
